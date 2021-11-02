f990415c877d8f661c5debe26ec8fa72efb06d58
dhyeysanghavi <55806338+dhyeysanghavi@users.noreply.github.com>
2021-11-03 00:10:10 +0530

    Update scraper.py

--- dummy-data-product/src/dependencies/scraping/scraper.py
+++ dummy-data-product/src/dependencies/scraping/scraper.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 driver = webdriver.Chrome(ChromeDriverManager().install())
 class Extract:
-    def extract():
+    def extract(self):
         driver.get('https://fred.stlouisfed.org/categories/33058')
         links=['https://fred.stlouisfed.org/series/TERMCBAUTO48NS','https://fred.stlouisfed.org/series/RIFLPBCIANM60NM','https://fred.stlouisfed.org/series/TERMAFCNCNSA']
         for i in links:
@@ -23,6 +23,6 @@ class Extract:
             if(i==('https://fred.stlouisfed.org/series/TERMAFCNCNSA')):
                 driver.get('https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=748&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=TERMAFCNCNSA&scale=left&cosd=1971-06-01&coed=2011-01-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2011-01-01&line_index=1&transformation=lin&vintage_date=2021-11-02&revision_date=2021-11-02&nd=1971-06-01')
 
-    if __name__ == '__main__':
-        obj=Extract()
-        obj.extract()
+if __name__ == '__main__':
+    obj=Extract()
+    obj.extract()
