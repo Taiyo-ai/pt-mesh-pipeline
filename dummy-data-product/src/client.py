db58da09854f1a7c34e1a8d49ab1ff846db30b8e
Md Tausif <codingf3ver@gmail.com>
2022-03-29 01:54:08 +0530

    Entire data pipeline

--- dummy-data-product/src/client.py
+++ dummy-data-product/src/client.py
@@ -1,35 +1,46 @@
-import dotenv
+# import dotenv
 import logging
 
 from datetime import datetime
 
 # Importing scraping and data processing modules
-# from dependencies.scraping.<file_name> import <class_name>
+from dependencies.scraping.scraper import Scraper
 # from dependencies.scraping.<file_name> import <class_name>
 # from dependencies.cleaning.<file_name> import <class_name>
 # from dependencies.geocoding.<file_name> import <class_name>
 # from dependencies.standardization.<file_name> import <class_name>
 
-dotenv.load_dotenv(".env")
+# dotenv.load_dotenv(".env")
 logging.basicConfig(level=logging.INFO)
 
 
 # In each step create an object of the class, initialize the class with 
 # required configuration and call the run method 
 def step_1():
+    """ This method will scrape the data from the website """
+    obj = Scraper()
+    obj.getMetadata()
     logging.info("Scraped Metadata")
 
-
 def step_2():
+    obj = Scraper()
+    obj.getScrapedData()
     logging.info("Scraped Main Data")
-
+    # return 
 
 def step_3():
+    obj = Scraper()
+    scraped_data = obj.getScrapedData()
+    obj.doCleanup(scraped_data)
     logging.info("Cleaned Main Data")
 
 
 def step_4():
-    logging.info("Geocoded Cleaned Data")
+    obj = Scraper()
+    scraped_data = obj.getScrapedData()
+    features= obj.doCleanup(scraped_data)
+    obj.generateCSV(features)
+    logging.info("Generated CSV")
 
 
 def step_5():
