f3b66c2522f78d0d8e918da0be2b1c01c427ef4b
BuTTeRvp <vying.vicky@gmail.com>
2022-09-15 13:13:52 +0530

    Vivek's scrapper (opentender eu)

---
+++ dummy-data-product/src/dependencies/utils/multicsv_to_one.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import glob
+import os
+
+
+# To merge all the CSV. 
+
+"""files = os.path.join("", "file*.csv")
+
+files = glob.glob(files)
+
+df = pd.concat(map(pd.read_csv, files), ignore_index=True)"""
