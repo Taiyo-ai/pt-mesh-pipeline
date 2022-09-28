cb4322c0f85d934d194a6b3ceee271c73001fc58
paja <firma1989>
2022-09-28 10:19:12 +0200

    Parsing done

--- eprocure/src/dependencies/scraping/eProcure.py
+++ eprocure/src/dependencies/scraping/eProcure.py
@@ -4,7 +4,7 @@ import requests
 import re
 import pandas as pd
 import pprint
-import time
+import json
 
 # from ..utils import load_config_yaml
 # from ..utils.bucket import (
@@ -37,7 +37,6 @@ class eProcureMetadataScraper:
                     "status": "",
                     # Critical Dates
                     "epublished_date": "",
-                    "epublished_date_": "",
                     "tender_opening_date": "",
                     "bid_opening_date": "",
                     "bid_submission_start_date": "",
@@ -66,88 +65,205 @@ class eProcureMetadataScraper:
                     "sessioned_url": "",
                 }
         # self.config = config
-        self.page_url_template = (
-            "https://eprocure.gov.in/cppp/latestactivetendersnew/cpppdata?page={}"
-        )
-        self.project_url_template = "https://www.pppinindia.gov.in/infrastructureindia/web/guest/project-list/{}"
+        self.page_url_templates = [
+            "https://eprocure.gov.in/cppp/latestactivetendersnew/cpppdata?page={}",
+            'https://eprocure.gov.in/cppp/latestactivecorrigendumsnew/cpppdata?page={}',
+            'https://eprocure.gov.in/cppp/globaltenders/cpppdata?page={}',
+            'https://eprocure.gov.in/cppp/globaltenders/mmpdata?page={}'
+            'https://eprocure.gov.in/cppp/highvaluetenders/cpppdata?page={}',
+        ]
 
     def get_project_urls(self):
         i = 0
         reached_last_pages = False
         project_urls = []
-        while not reached_last_pages:
-            print(f"Getting project_ids from page {i + 1}")
-            page_url = self.page_url_template.format(i)
 
-            session = requests.Session()
-            response = session.get(page_url, headers=self.header)
-            html_data = response.text
+        for page_url_template in self.page_url_templates:
+            file_name = re.search('e\.gov\.in\/cppp\/([^?]*)', page_url_template).group(1).replace('/', '_')
 
-            temp_list = self.get_urls_from_page(html_data)
+            # with open(file_name + '_.json', 'w') as json_file:
+            #     json_file.write('[')
 
-            for t in temp_list:
-                self.parse_url(t, session)
+            while not reached_last_pages:
+                print(f"Getting project_ids from page {i + 1}")
+                page_url = page_url_template.format(i)
+                session = requests.Session()
+                response = session.get(page_url, headers=self.header)
+                html_data = response.text
 
+                temp_list = self.get_urls_from_page(html_data)
 
-            i = i + 1
-            if temp_list is None or i == 3:
-                reached_last_pages = True
-                print("All pages scraped")
-            else:
-                project_urls.extend(temp_list)
-        return project_urls
+                for t in temp_list:
+                    self.parse_url(t, session, file_name)
+                i = i + 1
 
-    def parse_url(self, url, session):
+                if temp_list is None:
+                    reached_last_pages = True
+                    print("All pages scraped")
+                else:
+                    project_urls.extend(temp_list)
+            return project_urls
+
+    def parse_url(self, url, session, file_name):
         while 1:
             try:
                 response = session.get(url, headers=self.header)
-                # print(response.text)
-
                 soup = bs4.BeautifulSoup(response.text, "lxml")
-                cid = re.search('https://eprocure.gov.in/cppp/tendersfullview/(.*)', url).group(1)
-                captcha_text = soup.select_one('[data-drupal-selector="edit-captcha-image"]')['alt']
-                captcha_sid = soup.select_one('[name="captcha_sid"]')['value']
-                captcha_token = soup.select_one('[name="captcha_token"]')['value']
+                try:
+                    cid = re.search('https://eprocure.gov.in/cppp/tendersfullview/(.*)', url).group(1)
+                    captcha_text = soup.select_one('[data-drupal-selector="edit-captcha-image"]')['alt']
+                    captcha_sid = soup.select_one('[name="captcha_sid"]')['value']
+                    captcha_token = soup.select_one('[name="captcha_token"]')['value']
 
-                part_to_add = re.search('https://eprocure.gov.in/cppp/tendersfullview/([^=]*).([^=]*)', url).group(2)
-                # print('Captcha ------ ', captcha_text, captcha_sid, captcha_token)
+                    part_to_add = re.search('https://eprocure.gov.in/cppp/tendersfullview/([^=]*).([^=]*)', url).group(2)
+                    # print('Captcha ------ ', captcha_text, captcha_sid, captcha_token)
 
-                data_payload = {
-                    'cid': cid,
-                    'captcha_sid': captcha_sid,
-                    'captcha_token': captcha_token,
-                    'captcha_response':	captcha_text,
-                    'op': "Submit",
-                    'form_build_id': "form-fClpmN0wqPLHx7STyiqrDJ4vyc1iFFqoNVQfyAInFlk",
-                    'form_id': "Tenderfullview_tenders"
-                }
-                pprint.pprint(data_payload)
-                session.post(url, data=data_payload, headers=self.header)
+                    data_payload = {
+                        'cid': cid,
+                        'captcha_sid': captcha_sid,
+                        'captcha_token': captcha_token,
+                        'captcha_response':	captcha_text,
+                        'op': "Submit",
+                        'form_build_id': "form-fClpmN0wqPLHx7STyiqrDJ4vyc1iFFqoNVQfyAInFlk",
+                        'form_id': "Tenderfullview_tenders"
+                    }
+                    session.post(url, data=data_payload, headers=self.header)
+                except:
+                    session.post(url, headers=self.header)
+                    part_to_add = ''
 
                 url_for_response = url+part_to_add
-                print(url_for_response)
                 tender_response = session.get(url_for_response, headers=self.header, verify=False)
-                # print(tender_response.text)
 
                 tender_soup = bs4.BeautifulSoup(tender_response.text, "lxml")
                 main_table = tender_soup.select('#tfullview [width="100%"]')
                 table_text_for_regex = ''
                 for t in main_table:
-                    clean_table_text = t.text.replace('\n\n\n', ' ').replace(':\n\n', ': ')
+                    clean_table_text = t.text.replace('\n\n\n', ' ').replace(':\n\n', ': ').replace('&amp#x0d', ' ')
                     table_text_for_regex += clean_table_text + '\n'
-                print(table_text_for_regex)
+                # print(table_text_for_regex)
 
-                self.page_data['organization_name'] = re.search('Organisation Name\n:(.*)', table_text_for_regex).group(1).strip()
-                self.page_data['organization_type'] = re.search('Organisation Type\n:(.*)', table_text_for_regex).group(1).strip()
-                self.page_data['title'] = re.search('Tender Title\n:(.*)', table_text_for_regex).group(1).strip()
-                # parse it ...
+                try:
+                    self.page_data['organization_name'] = re.search('Organisation Name\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['organization_name'] = ''
+
+                try:
+                    self.page_data['organization_type'] = re.search('Organisation Type\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['organization_type'] = ''
+
+                try:
+                    self.page_data['title'] = re.search('Tender Title\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['title'] = ''
+
+                try:
+                    self.page_data['tender_reference_number'] = re.search('Tender Reference Number\n:\n(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['tender_reference_number'] = ''
+
+                try:
+                    self.page_data['tender_type'] = re.search('Tender Type\n:\n(.*) Tender', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['tender_type'] = ''
+
+                try:
+                    self.page_data['tender_category'] = re.search('Tender Category\n:\n(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['tender_category'] = ''
+
+                try:
+                    self.page_data['product_category'] = re.search('Product Category\n:\n(.*) Product', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['product_category'] = ''
+
+                try:
+                    self.page_data['product_sub_category'] = re.search('Product Sub-Category\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['product_sub_category'] = ''
+
+
+                if 'Tender Fee' in self.page_data['product_sub_category']:
+                    self.page_data['product_sub_category'] = ''
+
+                try:
+                    self.page_data['location'] = re.search('Location.*\n:\n(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['location'] = ''
+
+                try:
+                    self.page_data['tender_fee'] = re.search('Tender Fee.*\n:(.*) EMD.*\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['tender_fee'] = ''
+
+                try:
+                    self.page_data['emd'] = re.search('Tender Fee.*\n:(.*) EMD.*\n:(.*)', table_text_for_regex).group(2).strip()
+                except:
+                    self.page_data['emd'] = ''
+
+                try:
+                    self.page_data['epublished_date'] = re.search('ePublished Date\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['epublished_date'] = ''
+
+                try:
+                    self.page_data['bid_opening_date'] = re.search('Bid Opening Date\n:(.*) Doc', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['bid_opening_date'] = ''
+
+                try:
+                    self.page_data['document_download_start_date'] = re.search('Document Download Start Date\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['document_download_start_date'] = ''
+
+                try:
+                    self.page_data['document_download_end_date'] = re.search('Document Download End Date\n:(.*) Bid', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['document_download_end_date'] = ''
+
+                try:
+                    self.page_data['bid_submission_start_date'] = re.search('Bid Submission Start Date\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['bid_submission_start_date'] = ''
+
+                try:
+                    self.page_data['bid_submission_end_date'] = re.search('Bid Submission End Date\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['bid_submission_end_date'] = ''
+
+                try:
+                    self.page_data['work_description'] = re.search('Work Description\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['work_description'] = ''
+
+                try:
+                    self.page_data['tender_document_link'] = re.search('Tender Document\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['tender_document_link'] = ''
+
+                try:
+                    self.page_data['inviting_authority_name'] = re.search('\n Name\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['inviting_authority_name'] = ''
+
+                try:
+                    self.page_data['inviting_authority_address'] = re.search('Address\n:(.*)', table_text_for_regex).group(1).strip()
+                except:
+                    self.page_data['inviting_authority_address'] = ''
+
+                self.page_data['sessioned_url'] = url
+
                 pprint.pprint(self.page_data)
-
-                # print(entire_text)
+                # with open(file_name + '_.json' , 'a') as json_file:
+                #     json.dump(self.page_data,json_file,indent=4)
+                # with open(file_name + '_.json' , 'a') as f:
+                #     f.write(',')
                 break
             except:
                 print('Try again')
                 continue
+
 
     @staticmethod
     def get_urls_from_page(html_data):
