59c071a1a6dd7606860e25900a5498b471df29ac
nitinr211 <60518308+nitinr211@users.noreply.github.com>
2022-11-19 20:58:53 -0500

    mock functions that would perform the cleaning
    would feed a list of tuples from the initially scraped data (for simplicity) in each of these steps. the encoding to json will be done last in the standardize function in a final implementation

---
+++ dummy-data-product/src/dependencies/cleaning/cleaning.py
@@ -0,0 +1,20 @@
+import pandas
+import rates
+
+def missing_value_adjustment(x):
+    if x.isnan==True:
+        x='undefined'
+
+def exchange_currency(value, country):
+    exc_rate = rates.set_index(['from_currency', 'to_currency', 'date'])['exc_rate']
+    company['converted'] = company.apply(convert_to_usd, axis=1)
+    def convert_to_usd(row):
+        return row['value'] * exc_rate[row['currency'], 'USD', row['date']]
+    
+
+def sector_mapping:
+
+def status_mapping:
+
+def country_code_mapper:
+
