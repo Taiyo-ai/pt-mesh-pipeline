f8e29b9dcf2fccdeb1d0766da1df0eaeff873bf4
Shaheen ck <107412376+shahck@users.noreply.github.com>
2023-06-16 23:00:15 +0530

    Create scraper.py

---
+++ dummy-data-product/src/scraper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+class Scraper:
+    def __init__(self):
+        # Initialize any required variables or configuration here
+        pass
+
+    def scrape_data(self):
+        # World Bank Evaluation and Ratings
+        wb_data = self.scrape_world_bank()
+        self.save_to_csv(wb_data, "world_bank.csv")
+
+        # China Procurement Sources
+        china_data = self.scrape_china_sources()
+        self.save_to_csv(china_data, "china_sources.csv")
+
+        # E-procurement Government of India
+        india_data = self.scrape_india_sources()
+        self.save_to_csv(india_data, "india_sources.csv")
+
+    def scrape_world_bank(self):
+        url = "https://ieg.worldbankgroup.org/data"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        # Extract data using BeautifulSoup selectors or methods
+        # ...
+
+        # Return the extracted data as a list of dictionaries
+        return extracted_data
+
+    def scrape_china_sources(self):
+        # Scrape data from China procurement sources
+        # ...
+
+    def scrape_india_sources(self):
+        # Scrape data from E-procurement Government of India
+        # ...
+
+    def save_to_csv(self, data, filename):
+        fieldnames = data[0].keys()
+
+        with open(filename, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
